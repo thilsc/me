c332024b64a0865efae148a2c29a94a821b0e325
Thiago Coutinho <thiago.coutinho@iob.com.br>
2025-04-30 12:58:21 -0300

    Rebase

---
+++ tasks/db/config.php
@@ -0,0 +1,26 @@
+<?php
+$config = array();
+$config['db'] = 'sqlite';
+$config['mysql.host'] = 'localhost';
+$config['mysql.db'] = 'mytinytodo';
+$config['mysql.user'] = 'user';
+$config['mysql.password'] = '';
+$config['prefix'] = 'mtt_';
+$config['url'] = '';
+$config['mtt_url'] = '';
+$config['title'] = 'Thiago Coutinho - Tarefas';
+$config['lang'] = 'en';
+$config['password'] = 'diamond';
+$config['smartsyntax'] = 1;
+$config['timezone'] = 'America/Sao_Paulo';
+$config['autotag'] = 1;
+$config['duedateformat'] = 1;
+$config['firstdayofweek'] = 0;
+$config['session'] = 'default';
+$config['clock'] = 24;
+$config['dateformat'] = 'M d, Y';
+$config['dateformat2'] = 'dd/mm/yyyy';
+$config['dateformatshort'] = 'd.m';
+$config['template'] = 'default';
+$config['showdate'] = 1;
+?>
